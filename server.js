4a821d398ec40572c4d1e14c587bd42b22b2b293
Alan Chu <ac2779@rit.edu>
2020-09-26 14:31:48 -0400

    Modified the simple_recipe object to also contain the recipe source

--- server.js
+++ server.js
@@ -1,46 +1,47 @@
-const express = require('express')
-const app = express()
-const request = require('request')
-const fs = require('fs')
+const express = require("express");
+const app = express();
+const request = require("request");
+const fs = require("fs");
 
 var edamamApi = "https://api.edamam.com/search";
 var app_id = "";
 var app_key = "";
 
 // Set app_id and app_key
-app_id = fs.readFileSync('app_id.txt');
-app_key = fs.readFileSync('app_key.txt');
+app_id = fs.readFileSync("app_id.txt");
+app_key = fs.readFileSync("app_key.txt");
 
-app.get('/test', (req,res) => {
-    res.send({hello: 'Hello, World!'});
+app.get("/test", (req, res) => {
+  res.send({ hello: "Hello, World!" });
 });
 
 function requestApi(res, ingr) {
-    var ingredients = ingr;
+  var ingredients = ingr;
 
-    var api_request = edamamApi +
-                    "?q=" + ingr +
-                    "&app_id=" + app_id +
-                    "&app_key=" + app_key;
-    
-    request(api_request, {json: true}, (error, response, body) => {
-        var recipe = body.hits[0].recipe;
-        var simple_recipe = {
-            "label": recipe.label,
-            "image": recipe.image,
-            "url": recipe.url,
-            "yield": recipe.yield,
-            "ingredients": recipe.ingredients,
-            "totalNutrients": recipe.totalNutrients,
-            "calories": recipe.calories
-        }
-        res.send(simple_recipe);
-    });
+  var api_request =
+    edamamApi + "?q=" + ingr + "&app_id=" + app_id + "&app_key=" + app_key;
+
+  request(api_request, { json: true }, (error, response, body) => {
+    var recipe = body.hits[0].recipe;
+    var simple_recipe = {
+      label: recipe.label,
+      image: recipe.image,
+      url: recipe.url,
+      yield: recipe.yield,
+      ingredients: recipe.ingredients,
+      totalNutrients: recipe.totalNutrients,
+      calories: recipe.calories,
+      source: recipe.source,
+    };
+    res.send(simple_recipe);
+  });
 }
 
-app.get('/api', (req, res) => {
-    requestApi(res, req.query.ingr);
+app.get("/api", (req, res) => {
+  requestApi(res, req.query.ingr);
 });
 
 port = process.env.PORT || 5000;
-app.listen(port, () => { console.log('Listening on port ${port}')});
+app.listen(port, () => {
+  console.log("Listening on port ${port}");
+});
