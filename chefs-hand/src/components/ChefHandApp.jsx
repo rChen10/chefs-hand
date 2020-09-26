d8c3a2a0034ca6045f5714dd933899e01313f7fb
rchen10 <ricky.chen.2@stonybrook.edu>
2020-09-26 03:03:14 -0400

    Made the form dynamic (submit still wip)

--- chefs-hand/src/components/ChefHandApp.jsx
+++ chefs-hand/src/components/ChefHandApp.jsx
@@ -2,14 +2,31 @@ import React, { Component } from "react";
 
 class ChefHandApp extends Component {
   state = {
-    urls: [],
+    ingredients: [""],
+    urls: []
   };
 
+  // Helper Functions
   apiState = (res) => {
-    this.state.urls.push(res.url);
-    this.setState(this.state);
+    this.setState((prevState) => ({
+      ingredients: prevState.ingredients,
+      urls: [...prevState.urls, res.url]
+    }))
   };
 
+  addIngredient = (e) => {
+    this.setState((prevState) => ({
+      ingredients: [...prevState.ingredients, ""],
+      urls: prevState.urls
+    }))
+  };
+
+  // Event Handlers
+  ingredientHandler = (event) => {
+    event.preventDefault();
+  };
+
+  // Backend functions
   componentDidMount() {
     // Call our fetch function below once the component mounts
     this.callBackendAPI()
@@ -28,9 +45,23 @@ class ChefHandApp extends Component {
   };
 
   render() {
+    let {ingredients} = this.state;
     return (
       <div>
-        <h1> lol </h1>
+        <h1> Chef's Hand </h1>
+        <form onSubmit={this.ingredientHandler}>
+          <label htmlFor="Ingredient">Ingredient</label>
+          {
+            ingredients.map((ing, indx) => {
+              let ingId = 'ingredient${indx+1}';
+              return(
+                <input type="text" name="Ingredient" id={ingId} />
+              );
+            })
+          }
+          <button onClick={this.addIngredient}> Add new ingredient</button>
+          <input type="submit" value="Submit" />
+        </form>
         <p>
           <a href={this.state.urls[0]}>Here is your recipe.</a>
         </p>
